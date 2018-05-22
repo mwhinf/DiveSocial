a584917c2e02fb87af20ea597134cce8f44b5449
mwhinf <mwhinf@umich.edu>
2018-05-22 12:06:19 -0700

    Initial Commit

---
+++ DiveLog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiveLog
+//
+//  Created by Michael Whinfrey on 5/22/18.
+//  Copyright © 2018 Michael Whinfrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
