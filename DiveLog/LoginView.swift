b389db3a3971cbe199cb0deab262e96bb85c3980
mwhinf <mwhinf@umich.edu>
2018-06-22 09:08:41 -0700

    Add .gitignore file

---
+++ DiveLog/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  DiveLog
+//
+//  Created by Michael Whinfrey on 5/30/18.
+//  Copyright © 2018 Michael Whinfrey. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DiveLog/MapMarkerWindow.swift
@@ -0,0 +1,21 @@
+//
+//  MapMarkerWindow.swift
+//  DiveLog
+//
+//  Created by Michael Whinfrey on 6/6/18.
+//  Copyright © 2018 Michael Whinfrey. All rights reserved.
+//
+
+import UIKit
+
+class MapMarkerWindow: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
