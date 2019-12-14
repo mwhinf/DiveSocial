3a368a685cf4d68e79576acad075d1a1334dec9d
Michael Whinfrey <michael@joinforge.com>
2019-12-13 18:47:52 -0800

    Add dummy dive data

--- DiveLog/View Controllers/DiveListController.swift
+++ DiveLog/View Controllers/DiveListController.swift
@@ -48,7 +48,6 @@ class DiveListController: UIViewController, UITableViewDelegate, UITableViewData
         fetchRequest.sortDescriptors = [sortDescriptor]
         
         do {
-                
             let diveList = try managedContext.fetch(fetchRequest)
 
             for (index, dive) in diveList.enumerated() {
@@ -71,6 +70,8 @@ class DiveListController: UIViewController, UITableViewDelegate, UITableViewData
         DispatchQueue.main.async
         { self.tableView.reloadData() }
         
+        if self.dives.isEmpty
+            { loadDummyData() }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {}
@@ -177,6 +178,168 @@ class DiveListController: UIViewController, UITableViewDelegate, UITableViewData
     }
         return [delete]
 }
+    
+    func loadDummyData(){
+        
+        let managedContext = coreDataManager.managedObjectContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "DiveInstance", in: managedContext)!
+        
+        let dive1 = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        dive1.setValue("1", forKeyPath: "diveNo")
+        dive1.setValue("11/2/15", forKeyPath: "date")
+        dive1.setValue("Chumphon Pinnacle", forKeyPath: "diveSite")
+        dive1.setValue("Koh Tao", forKeyPath: "location")
+        dive1.setValue("Thailand", forKeyPath: "country")
+        dive1.setValue("23 m", forKeyPath: "depth")
+        dive1.setValue("45 min", forKeyPath: "bottomTime")
+        dive1.setValue(10.17148333, forKeyPath: "latitude")
+        dive1.setValue(99.77835, forKeyPath: "longitude")
+        dive1.setValue("4:15 AM", forKeyPath: "timeIn")
+        dive1.setValue("5.00 AM", forKeyPath: "timeOut")
+        dive1.setValue("", forKeyPath: "diveType")
+        dive1.setValue("", forKeyPath: "safetyStopDepth")
+        dive1.setValue("", forKeyPath: "safetyStopDuration")
+        dive1.setValue("", forKeyPath: "surfaceInterval")
+        dive1.setValue("", forKeyPath: "diveMasterName")
+        dive1.setValue("", forKeyPath: "diveMasterNum")
+        dive1.setValue("", forKeyPath: "airTemp")
+        dive1.setValue("", forKeyPath: "waterTemp")
+        dive1.setValue("", forKeyPath: "visibility")
+        dive1.setValue("", forKeyPath: "weight")
+        dive1.setValue("", forKeyPath: "startTankPressure")
+        dive1.setValue("", forKeyPath: "endTankPressure")
+        dive1.setValue("", forKeyPath: "diveNotes")
+        dive1.setValue(1446516480.0, forKeyPath: "timeInterval")
+        
+        
+        let dive2 = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        dive2.setValue("2", forKeyPath: "diveNo")
+        dive2.setValue("2/14/16", forKeyPath: "date")
+        dive2.setValue("Victory Wreck", forKeyPath: "diveSite")
+        dive2.setValue("West Java", forKeyPath: "location")
+        dive2.setValue("Indonesia", forKeyPath: "country")
+        dive2.setValue("21 m", forKeyPath: "depth")
+        dive2.setValue("52 min", forKeyPath: "bottomTime")
+        dive2.setValue(-5.972547505, forKeyPath: "latitude")
+        dive2.setValue(105.8627155, forKeyPath: "longitude")
+        dive2.setValue("8:10 AM", forKeyPath: "timeIn")
+        dive2.setValue("9:02 AM", forKeyPath: "timeOut")
+        dive2.setValue("", forKeyPath: "diveType")
+        dive2.setValue("", forKeyPath: "safetyStopDepth")
+        dive2.setValue("", forKeyPath: "safetyStopDuration")
+        dive2.setValue("", forKeyPath: "surfaceInterval")
+        dive2.setValue("", forKeyPath: "diveMasterName")
+        dive2.setValue("", forKeyPath: "diveMasterNum")
+        dive2.setValue("", forKeyPath: "airTemp")
+        dive2.setValue("", forKeyPath: "waterTemp")
+        dive2.setValue("", forKeyPath: "visibility")
+        dive2.setValue("", forKeyPath: "weight")
+        dive2.setValue("", forKeyPath: "startTankPressure")
+        dive2.setValue("", forKeyPath: "endTankPressure")
+        dive2.setValue("", forKeyPath: "diveNotes")
+        dive2.setValue(1455502770.0, forKeyPath: "timeInterval")
+        
+        
+        let dive3 = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        dive3.setValue("3", forKeyPath: "diveNo")
+        dive3.setValue("4/7/17", forKeyPath: "date")
+        dive3.setValue("Oluhuta", forKeyPath: "diveSite")
+        dive3.setValue("Gorontalo", forKeyPath: "location")
+        dive3.setValue("Indonesia", forKeyPath: "country")
+        dive3.setValue("15 m", forKeyPath: "depth")
+        dive3.setValue("58 min", forKeyPath: "bottomTime")
+        dive3.setValue(0.419622485, forKeyPath: "latitude")
+        dive3.setValue(123.1446362, forKeyPath: "longitude")
+        dive3.setValue("8:30 AM", forKeyPath: "timeIn")
+        dive3.setValue("9:28 AM", forKeyPath: "timeOut")
+        dive3.setValue("", forKeyPath: "diveType")
+        dive3.setValue("", forKeyPath: "safetyStopDepth")
+        dive3.setValue("", forKeyPath: "safetyStopDuration")
+        dive3.setValue("", forKeyPath: "surfaceInterval")
+        dive3.setValue("", forKeyPath: "diveMasterName")
+        dive3.setValue("", forKeyPath: "diveMasterNum")
+        dive3.setValue("", forKeyPath: "airTemp")
+        dive3.setValue("", forKeyPath: "waterTemp")
+        dive3.setValue("", forKeyPath: "visibility")
+        dive3.setValue("", forKeyPath: "weight")
+        dive3.setValue("", forKeyPath: "startTankPressure")
+        dive3.setValue("", forKeyPath: "endTankPressure")
+        dive3.setValue("", forKeyPath: "diveNotes")
+        dive3.setValue(1491614781.0, forKeyPath: "timeInterval")
+    
+        
+        let dive4 = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        dive4.setValue("4", forKeyPath: "diveNo")
+        dive4.setValue("12/22/17", forKeyPath: "date")
+        dive4.setValue("Lighthouse Pinnacle", forKeyPath: "diveSite")
+        dive4.setValue("Koh Tao", forKeyPath: "location")
+        dive4.setValue("Thailand", forKeyPath: "country")
+        dive4.setValue("20 m", forKeyPath: "depth")
+        dive4.setValue("42 min", forKeyPath: "bottomTime")
+        dive4.setValue(10.12089317, forKeyPath: "latitude")
+        dive4.setValue(99.84523207, forKeyPath: "longitude")
+        dive4.setValue("10:30 AM", forKeyPath: "timeIn")
+        dive4.setValue("11:12 AM", forKeyPath: "timeOut")
+        dive4.setValue("", forKeyPath: "diveType")
+        dive4.setValue("", forKeyPath: "safetyStopDepth")
+        dive4.setValue("", forKeyPath: "safetyStopDuration")
+        dive4.setValue("", forKeyPath: "surfaceInterval")
+        dive4.setValue("", forKeyPath: "diveMasterName")
+        dive4.setValue("", forKeyPath: "diveMasterNum")
+        dive4.setValue("", forKeyPath: "airTemp")
+        dive4.setValue("", forKeyPath: "waterTemp")
+        dive4.setValue("", forKeyPath: "visibility")
+        dive4.setValue("", forKeyPath: "weight")
+        dive4.setValue("", forKeyPath: "startTankPressure")
+        dive4.setValue("", forKeyPath: "endTankPressure")
+        dive4.setValue("", forKeyPath: "diveNotes")
+        dive4.setValue(1576290917.198333, forKeyPath: "timeInterval")
+        
+        
+        let dive5 = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        dive5.setValue("5", forKeyPath: "diveNo")
+        dive5.setValue("3/12/18", forKeyPath: "date")
+        dive5.setValue("Intan Wreck", forKeyPath: "diveSite")
+        dive5.setValue("Karimunjawa National Park", forKeyPath: "location")
+        dive5.setValue("Indonesia", forKeyPath: "country")
+        dive5.setValue("31 m", forKeyPath: "depth")
+        dive5.setValue("39 min", forKeyPath: "bottomTime")
+        dive5.setValue(-4.406666667, forKeyPath: "latitude")
+        dive5.setValue(106.6833333, forKeyPath: "longitude")
+        dive5.setValue("7:30 AM", forKeyPath: "timeIn")
+        dive5.setValue("8:09 AM", forKeyPath: "timeOut")
+        dive5.setValue("", forKeyPath: "diveType")
+        dive5.setValue("", forKeyPath: "safetyStopDepth")
+        dive5.setValue("", forKeyPath: "safetyStopDuration")
+        dive5.setValue("", forKeyPath: "surfaceInterval")
+        dive5.setValue("", forKeyPath: "diveMasterName")
+        dive5.setValue("", forKeyPath: "diveMasterNum")
+        dive5.setValue("", forKeyPath: "airTemp")
+        dive5.setValue("", forKeyPath: "waterTemp")
+        dive5.setValue("", forKeyPath: "visibility")
+        dive5.setValue("", forKeyPath: "weight")
+        dive5.setValue("", forKeyPath: "startTankPressure")
+        dive5.setValue("", forKeyPath: "endTankPressure")
+        dive5.setValue("", forKeyPath: "diveNotes")
+        dive5.setValue(1520905153.0, forKeyPath: "timeInterval")
+        
+        do {
+            try managedContext.save()
+            dives.append(dive1)
+            dives.append(dive2)
+            dives.append(dive3)
+            dives.append(dive4)
+            dives.append(dive5)
+           }
+        catch let error as NSError
+            { print("Could not save. \(error), \(error.userInfo)") }
+    }
 }
 
 extension UITextField {
